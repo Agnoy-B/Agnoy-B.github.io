eb2945a762b48ec7447b753e5ab98969d15fed47
Agnoy-B <2276664234@qq.com>
2026-05-14 08:22:36 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'my-site-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
+// 安装 Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 拦截请求，优先使用缓存
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
